8af853e04eb66198af003945ae3dddf5e1b77143
Billy Mech <wmech16@gmail.com>
2017-08-27 17:25:22 -0400

    google auth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ name: 'Billy' });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: 'http://localhost:5000/auth/google/callback'
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
 
-const PORT = process.env.PORT || 5000;
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT);
